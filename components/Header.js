4c709397f715623bc46de4ef73bb43714a17d2b5
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-02-25 20:59:51 +0700

    Update header template

--- components/Header.js
+++ components/Header.js
@@ -1,36 +1,59 @@
-import React from 'react';
 import Link from 'next/link';
-
-import { MenuIcon } from '@heroicons/react/solid';
+import React from 'react';
 
 const Header = () => {
   return (
-    <nav className='shadow-sm'>
-      <div className='flex container h-[56px] px-2 mx-auto'>
-        <div className='flex flex-row items-center flex-1 space-x-1'>
-          <button className='rounded cursor-pointer active:bg-gray-300'>
-            <MenuIcon width='32px' height='32px' className='sm:hidden' />
-          </button>
-          <Link href='/'>
-            <a className='inline-flex text-2xl font-semibold text-green-700 cursor-pointer'>
+    <>
+      <nav className='navbar navbar-light'>
+        <div className='container'>
+          <Link href='/' passHref>
+            <a className='navbar-brand' href='index.html'>
               conduit
             </a>
           </Link>
-        </div>
+          <ul className='nav navbar-nav pull-xs-right'>
+            <Link href='/' passHref>
+              <li className='nav-item'>
+                <a className='nav-link active' href=''>
+                  Home
+                </a>
+              </li>
+            </Link>
+            <Link href='/' passHref>
+              <li className='nav-item'>
+                <a className='nav-link' href=''>
+                  <i className='ion-compose'></i>&nbsp;New Article
+                </a>
+              </li>
+            </Link>
 
-        <div className='flex items-center space-x-6'>
-          <Link href='/'>
-            <a className='text-gray-400'>Home</a>
-          </Link>
-          <Link href='/login'>
-            <a className='text-gray-400'>Sign in</a>
-          </Link>
-          <Link href='/register'>
-            <a className='text-gray-400'>Sign up</a>
-          </Link>
+            <Link href='/' passHref>
+              <li className='nav-item'>
+                <a className='nav-link' href=''>
+                  <i className='ion-gear-a'></i>&nbsp;Settings
+                </a>
+              </li>
+            </Link>
+
+            <Link href='/' passHref>
+              <li className='nav-item'>
+                <a className='nav-link' href=''>
+                  Sign in
+                </a>
+              </li>
+            </Link>
+
+            <Link href='/' passHref>
+              <li className='nav-item'>
+                <a className='nav-link' href=''>
+                  Sign up
+                </a>
+              </li>
+            </Link>
+          </ul>
         </div>
-      </div>
-    </nav>
+      </nav>
+    </>
   );
 };
 
