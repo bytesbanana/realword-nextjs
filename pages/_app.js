d3b2b43936ce99ab67fd8257391cddb3766dc082
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-02-25 20:30:47 +0700

    remove recoil dependency

--- pages/_app.js
+++ pages/_app.js
@@ -1,13 +1,12 @@
 import 'styles/globals.css';
 import Header from 'components/Header';
-import { RecoilRoot } from 'recoil';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <RecoilRoot>
+    <>
       <Header />
       <Component {...pageProps} />
-    </RecoilRoot>
+    </>
   );
 }
 
