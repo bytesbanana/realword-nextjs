fcd45bba7a2433e259c415f2f1db211a5e3d04df
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-02-25 21:00:01 +0700

    add head tag

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,15 @@
 import 'styles/globals.css';
 import Header from 'components/Header';
+import Head from 'next/head';
+import Link from 'next/link';
 
 function MyApp({ Component, pageProps }) {
   return (
     <>
+      <Head>
+        <meta charSet='utf-8' />
+        <title>Conduit</title>
+      </Head>
       <Header />
       <Component {...pageProps} />
     </>
