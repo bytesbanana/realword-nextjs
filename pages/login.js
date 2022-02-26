3375ec12c193f108346044ab5586cc42b6f60452
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-02-26 20:48:46 +0700

    Update login logic

--- pages/login.js
+++ pages/login.js
@@ -1,42 +1,90 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import Link from 'next/link';
-import axios from 'axios';
+import { useRouter } from 'next/router';
+import UserAPI from 'lib/api/user';
+import { useSelector } from 'react-redux';
+import { useDispatch } from 'react-redux';
+import { appActions } from '../store/app';
+import { authActions } from '../store/auth';
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const router = useRouter();
+  const [errors, setErrors] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const loading = useSelector((state) => state.app.loading);
+  const loginUser = useSelector((state) => state.auth.user);
+  const dispatch = useDispatch();
 
-  const loginHandler = () => {};
+  const showErrors = () => {
+    <ul className='error-messages'>
+      {Object.keys(errors).map((key) => (
+        <li key={key}>
+          `${key} ${errors[key]}
+        </li>
+      ))}
+    </ul>;
+  };
+
+  const loginHandler = async (e) => {
+    e.preventDefault();
+    dispatch(appActions.showLoading());
+
+    const response = await UserAPI.login(email, password);
+    const data = await response.json();
+
+    dispatch(appActions.hideLoading());
+    if (response.ok) {
+      localStorage.setItem('user', JSON.stringify(data.user));
+      dispatch(authActions.setUser(data.user));
+      router.push('/');
+    } else {
+      setErrors(data.errors);
+    }
+  };
 
   return (
-    <div className='container mx-auto'>
-      <form className='flex flex-col max-w-sm p-10 mx-auto space-y-4'>
-        <h1 className='mb-4 text-4xl font-light text-center'>Sign in</h1>
-        <input
-          type='email'
-          placeholder='Email'
-          className='form-input'
-          name='email'
-          onChange={(e) => setEmail(e.target.value)}
-        />
-        <input
-          name='password'
-          type='password'
-          placeholder='Password'
-          className='form-input'
-          onChange={(e) => setPassword(e.target.value)}
-        />
-        <div className='flex items-center'>
-          <Link href='/register'>
-            <a className='flex-auto text-sm text-green-700'>
-              {"Don't have an account?"}
-            </a>
-          </Link>
-          <button type='button' className='btn-primary' onClick={loginHandler}>
-            Sign in
-          </button>
+    <div className='auth-page'>
+      <div className='container page'>
+        <div className='row'>
+          <div className='col-md-6 offset-md-3 col-xs-12'>
+            <h1 className='text-xs-center'>Sign in</h1>
+            <p className='text-xs-center'>
+              <Link href='/register'>
+                <a>Or create new account</a>
+              </Link>
+            </p>
+            {errors && showErrors()}
+
+            <form onSubmit={loginHandler}>
+              <fieldset className='form-group'>
+                <input
+                  className='form-control form-control-lg'
+                  type='text'
+                  placeholder='Email'
+                  onChange={(e) => setEmail(e.target.value)}
+                  disabled={loading}
+                />
+              </fieldset>
+              <fieldset className='form-group'>
+                <input
+                  className='form-control form-control-lg'
+                  type='password'
+                  placeholder='Password'
+                  onChange={(e) => setPassword(e.target.value)}
+                  disabled={loading}
+                />
+              </fieldset>
+              <button
+                className='btn btn-lg btn-primary pull-xs-right'
+                disabled={loading}
+              >
+                Sign in
+              </button>
+            </form>
+          </div>
         </div>
-      </form>
+      </div>
     </div>
   );
 };
