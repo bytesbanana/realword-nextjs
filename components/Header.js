c529340fd3c723f9fdb04f71a87c57f15c77aba0
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-02-27 19:42:20 +0700

    Add active link class name

--- components/Header.js
+++ components/Header.js
@@ -1,62 +1,77 @@
 import Link from 'next/link';
 import React from 'react';
 import { useSelector } from 'react-redux';
+import Image from 'next/image';
+import ActiveLink from './ActiveLink';
 
 const Header = () => {
   const user = useSelector((state) => state.auth.user);
-
   return (
     <>
       <nav className='navbar navbar-light'>
         <div className='container'>
-          <Link href='/' passHref>
-            <a className='navbar-brand' href='index.html'>
-              conduit
-            </a>
-          </Link>
+          <ActiveLink activeClassName='active' href='/' passHref>
+            <a className='navbar-brand'>conduit</a>
+          </ActiveLink>
           <ul className='nav navbar-nav pull-xs-right'>
-            <Link href='/' passHref>
+            <ActiveLink activeClassName='active' href='/' passHref>
               <li className='nav-item'>
-                <a className='nav-link active' href=''>
-                  Home
-                </a>
+                <a className='nav-link'>Home</a>
               </li>
-            </Link>
+            </ActiveLink>
             {user && (
-              <Link href='/' passHref>
+              <ActiveLink activeClassName='active' href='/editor' passHref>
                 <li className='nav-item'>
-                  <a className='nav-link' href=''>
+                  <a className='nav-link'>
                     <i className='ion-compose'></i>&nbsp;New Article
                   </a>
                 </li>
-              </Link>
+              </ActiveLink>
             )}
 
             {user && (
-              <Link href='/' passHref>
-                <li className='nav-item'>
-                  <a className='nav-link' href=''>
-                    <i className='ion-gear-a'></i>&nbsp;Settings
-                  </a>
-                </li>
-              </Link>
+              <>
+                <ActiveLink activeClassName='active' href='/' passHref>
+                  <li className='nav-item'>
+                    <a className='nav-link'>
+                      <i className='ion-gear-a'></i>&nbsp;Settings
+                    </a>
+                  </li>
+                </ActiveLink>
+
+                <ActiveLink activeClassName='active' href='/register' passHref>
+                  <li className='nav-item'>
+                    <a className='nav-link' style={{ display: 'flex' }}>
+                      <Image
+                        className='user-pic'
+                        src='https://api.realworld.io/images/smiley-cyrus.jpeg'
+                        alt='User picture'
+                        width={26}
+                        height={26}
+                        objectFit='cover'
+                      />
+                      <span style={{ marginLeft: '5px' }}>
+                        {user?.username}
+                      </span>
+                    </a>
+                  </li>
+                </ActiveLink>
+              </>
             )}
 
             {!user && (
               <>
-                <Link href='/login' passHref>
+                <ActiveLink activeClassName='active' href='/login' passHref>
                   <li className='nav-item'>
                     <a className='nav-link'>Sign in</a>
                   </li>
-                </Link>
+                </ActiveLink>
 
-                <Link href='/register' passHref>
+                <ActiveLink activeClassName='active' href='/register' passHref>
                   <li className='nav-item'>
-                    <a className='nav-link' href=''>
-                      Sign up
-                    </a>
+                    <a className='nav-link'>Sign up</a>
                   </li>
-                </Link>
+                </ActiveLink>
               </>
             )}
           </ul>
