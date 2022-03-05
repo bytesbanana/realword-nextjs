9af56cca9d46e9f4f3a2ef6a42394504042047a3
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-03-05 22:51:37 +0700

    bind profile data to ui

--- pages/profile.js
+++ pages/profile.js
@@ -1,22 +1,58 @@
-import React from 'react';
+import React, { useRef, useState } from 'react';
+import UserAPI from 'lib/api/user';
+import Image from 'next/image';
+import { useSelector } from 'react-redux';
 
-const Profile = () => {
+const Profile = ({ profile }) => {
+  const currentUser = useSelector((state) => state.auth.user);
+  const menuDataRef = useRef(['My Articles', 'Favorted Articles']);
+  const [selectedTab, setSelectedTab] = useState(0);
+
+  const editProfileHandler = () => {};
+
+  const followUserHandler = () => {};
+
+  const renderProfileAction = () => {
+    if (currentUser?.username === profile.username) {
+      return (
+        <button
+          className='btn btn-sm btn-outline-secondary action-btn'
+          onClick={editProfileHandler}
+        >
+          <i className='ion-gear-a'></i>
+          &nbsp; Edit Profile Settings
+        </button>
+      );
+    }
+
+    return (
+      <button
+        className='btn btn-sm btn-outline-secondary action-btn'
+        onClick={followUserHandler}
+      >
+        <i className='ion-plus-round'></i>
+        &nbsp; Follow {profile.username}
+      </button>
+    );
+  };
+
   return (
     <div className='profile-page'>
       <div className='user-info'>
         <div className='container'>
           <div className='row'>
             <div className='col-xs-12 col-md-10 offset-md-1'>
-              <img src='http://i.imgur.com/Qr71crq.jpg' className='user-img' />
-              <h4>Eric Simons</h4>
-              <p>
-                Cofounder @GoThinkster, lived in Aol's HQ for a few months,
-                kinda looks like Peeta from the Hunger Games
-              </p>
-              <button className='btn btn-sm btn-outline-secondary action-btn'>
-                <i className='ion-plus-round'></i>
-                &nbsp; Follow Eric Simons
-              </button>
+              <Image
+                className='user-img'
+                src={profile.image}
+                width={100}
+                height={100}
+                alt={profile.Profileusername}
+              />
+
+              <h4>{profile.username}</h4>
+              <p>{profile.bio}</p>
+              {renderProfileAction()}
             </div>
           </div>
         </div>
@@ -27,68 +63,17 @@ const Profile = () => {
           <div className='col-xs-12 col-md-10 offset-md-1'>
             <div className='articles-toggle'>
               <ul className='nav nav-pills outline-active'>
-                <li className='nav-item'>
-                  <a className='nav-link active' href=''>
-                    My Articles
-                  </a>
-                </li>
-                <li className='nav-item'>
-                  <a className='nav-link' href=''>
-                    Favorited Articles
-                  </a>
-                </li>
+                {menuDataRef.current.map((menu, i) => (
+                  <li className='nav-item' key={i}>
+                    <a
+                      className={`nav-link ${i == selectedTab && 'active'}`}
+                      onClick={() => setSelectedTab(i)}
+                    >
+                      {menu}
+                    </a>
+                  </li>
+                ))}
               </ul>
-            </div>
-
-            <div className='article-preview'>
-              <div className='article-meta'>
-                <a href=''>
-                  <img src='http://i.imgur.com/Qr71crq.jpg' />
-                </a>
-                <div className='info'>
-                  <a href='' className='author'>
-                    Eric Simons
-                  </a>
-                  <span className='date'>January 20th</span>
-                </div>
-                <button className='btn btn-outline-primary btn-sm pull-xs-right'>
-                  <i className='ion-heart'></i> 29
-                </button>
-              </div>
-              <a href='' className='preview-link'>
-                <h1>How to build webapps that scale</h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-              </a>
-            </div>
-
-            <div className='article-preview'>
-              <div className='article-meta'>
-                <a href=''>
-                  <img src='http://i.imgur.com/N4VcUeJ.jpg' />
-                </a>
-                <div className='info'>
-                  <a href='' className='author'>
-                    Albert Pai
-                  </a>
-                  <span className='date'>January 20th</span>
-                </div>
-                <button className='btn btn-outline-primary btn-sm pull-xs-right'>
-                  <i className='ion-heart'></i> 32
-                </button>
-              </div>
-              <a href='' className='preview-link'>
-                <h1>
-                  The song you won't ever stop singing. No matter how hard you
-                  try.
-                </h1>
-                <p>This is the description for the post.</p>
-                <span>Read more...</span>
-                <ul className='tag-list'>
-                  <li className='tag-default tag-pill tag-outline'>Music</li>
-                  <li className='tag-default tag-pill tag-outline'>Song</li>
-                </ul>
-              </a>
             </div>
           </div>
         </div>
@@ -98,3 +83,13 @@ const Profile = () => {
 };
 
 export default Profile;
+
+export async function getStaticProps() {
+  const profile = await UserAPI.profile();
+
+  return {
+    props: {
+      ...profile,
+    },
+  };
+}
