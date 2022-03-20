85123511f1e2d13031daea8cec737d66a7818020
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-03-20 14:08:25 +0700

    implement fav/unfav preview article

--- components/articles/ArticlePreviewListItem.js
+++ components/articles/ArticlePreviewListItem.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Image from 'next/image';
-import { HeartIcon } from '@heroicons/react/solid';
-
 import Link from 'next/link';
 import { formatDate } from 'lib/date';
 
-const ArticlePreviewLitItem = ({ article }) => {
+import ArticleAPI from 'lib/api/article';
+
+const ArticlePreviewLitItem = (props) => {
+  const [article, setArticle] = useState(props.article);
+  const { slug } = article;
+
+  const favArticleHandler = async () => {
+    const data = await ArticleAPI.favoriteArticle(slug);
+    setArticle(data.article);
+  };
+
+  const unfavArticleHandler = async () => {
+    const data = await ArticleAPI.unfavoriteArticle(slug);
+    setArticle(data.article);
+  };
+
+  console.log(article.favorited);
+
   return (
     <div className='article-preview'>
       <div className='article-meta'>
         <a href='profile.html'>
           <Image
-            src={article.author.image}
+            src={article?.author?.image}
             alt={article.username}
             width={32}
             height={32}
@@ -24,7 +39,12 @@ const ArticlePreviewLitItem = ({ article }) => {
           </a>
           <span className='date'> {formatDate(article.createdAt)}</span>
         </div>
-        <button className='btn btn-outline-primary btn-sm pull-xs-right'>
+        <button
+          className={`btn btn-outline-primary btn-sm pull-xs-right ${
+            article.favorited ? 'active' : ''
+          }`}
+          onClick={article.favorited ? unfavArticleHandler : favArticleHandler}
+        >
           <i className='ion-heart'></i> {article.favoritesCount}
         </button>
       </div>
