06142bb0f787d760b2e5b4fb433e97ac30cf906e
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-03-22 16:19:28 +0700

    update user data storage to state

--- components/Header.js
+++ components/Header.js
@@ -5,26 +5,30 @@ import Image from 'next/image';
 import ActiveLink from './ActiveLink';
 
 const Header = () => {
-  const user = JSON.parse(localStorage.getItem('user'))
+  const [user, setUser] = React.useState();
+
+  React.useEffect(() => {
+    setUser(JSON.parse(localStorage.getItem('user')));
+  }, []);
 
   return (
     <>
-      <nav className="navbar navbar-light">
-        <div className="container">
-          <ActiveLink activeClassName="active" href="/" passHref>
-            <a className="navbar-brand">conduit</a>
+      <nav className='navbar navbar-light'>
+        <div className='container'>
+          <ActiveLink activeClassName='active' href='/' passHref>
+            <a className='navbar-brand'>conduit</a>
           </ActiveLink>
-          <ul className="nav navbar-nav pull-xs-right">
-            <ActiveLink activeClassName="active" href="/" passHref>
-              <li className="nav-item">
-                <a className="nav-link">Home</a>
+          <ul className='nav navbar-nav pull-xs-right'>
+            <ActiveLink activeClassName='active' href='/' passHref>
+              <li className='nav-item'>
+                <a className='nav-link'>Home</a>
               </li>
             </ActiveLink>
             {user && (
-              <ActiveLink activeClassName="active" href="/editor" passHref>
-                <li className="nav-item">
-                  <a className="nav-link">
-                    <i className="ion-compose"></i>&nbsp;New Article
+              <ActiveLink activeClassName='active' href='/editor' passHref>
+                <li className='nav-item'>
+                  <a className='nav-link'>
+                    <i className='ion-compose'></i>&nbsp;New Article
                   </a>
                 </li>
               </ActiveLink>
@@ -32,26 +36,32 @@ const Header = () => {
 
             {user && (
               <>
-                <ActiveLink activeClassName="active" href="/settings" passHref>
-                  <li className="nav-item">
-                    <a className="nav-link">
-                      <i className="ion-gear-a"></i>&nbsp;Settings
+                <ActiveLink activeClassName='active' href='/settings' passHref>
+                  <li className='nav-item'>
+                    <a className='nav-link'>
+                      <i className='ion-gear-a'></i>&nbsp;Settings
                     </a>
                   </li>
                 </ActiveLink>
 
-                <ActiveLink activeClassName="active" href={`/profile/${user.username}`} passHref>
-                  <li className="nav-item">
-                    <a className="nav-link" style={{ display: 'flex' }}>
+                <ActiveLink
+                  activeClassName='active'
+                  href={`/profile/${user.username}`}
+                  passHref
+                >
+                  <li className='nav-item'>
+                    <a className='nav-link' style={{ display: 'flex' }}>
                       <Image
-                        className="user-pic"
-                        src="https://api.realworld.io/images/smiley-cyrus.jpeg"
-                        alt="User picture"
+                        className='user-pic'
+                        src='https://api.realworld.io/images/smiley-cyrus.jpeg'
+                        alt='User picture'
                         width={26}
                         height={26}
-                        objectFit="cover"
+                        objectFit='cover'
                       />
-                      <span style={{ marginLeft: '5px' }}>{user?.username}</span>
+                      <span style={{ marginLeft: '5px' }}>
+                        {user?.username}
+                      </span>
                     </a>
                   </li>
                 </ActiveLink>
@@ -60,15 +70,15 @@ const Header = () => {
 
             {!user && (
               <>
-                <ActiveLink activeClassName="active" href="/login" passHref>
-                  <li className="nav-item">
-                    <a className="nav-link">Sign in</a>
+                <ActiveLink activeClassName='active' href='/login' passHref>
+                  <li className='nav-item'>
+                    <a className='nav-link'>Sign in</a>
                   </li>
                 </ActiveLink>
 
-                <ActiveLink activeClassName="active" href="/register" passHref>
-                  <li className="nav-item">
-                    <a className="nav-link">Sign up</a>
+                <ActiveLink activeClassName='active' href='/register' passHref>
+                  <li className='nav-item'>
+                    <a className='nav-link'>Sign up</a>
                   </li>
                 </ActiveLink>
               </>
