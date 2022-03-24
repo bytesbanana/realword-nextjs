858f48ef121128d5786ad12f466e294ed80fe85e
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-03-24 22:28:10 +0700

    Load localstorage to store

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,17 @@
 import React from 'react';
 import { useDispatch } from 'react-redux';
+import { authActions } from 'store/auth';
 import Header from './Header';
 
 const Layout = ({ children }) => {
   const dispatch = useDispatch();
+
+  React.useEffect(() => {
+    if (typeof window !== 'undefined') {
+      const user = JSON.parse(localStorage.getItem('user'));
+      dispatch(authActions.setUser(user));
+    }
+  }, [dispatch]);
 
   return (
     <>
