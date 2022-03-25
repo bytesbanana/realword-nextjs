b1efdd557cfbb17a238b1b3819f4a6fb2ac7d3eb
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-03-25 17:08:57 +0700

    Add bar of  progress

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,23 @@
 import 'styles/globals.css';
 import Head from 'next/head';
+import ProgressBar from '@badrap/bar-of-progress';
+import Router from 'next/router';
 
 import { Provider } from 'react-redux';
 import store from 'store';
 import Layout from 'components/Layout';
+
+
+const progress = new ProgressBar({
+  size: 4,
+  color: '#5cb85c',
+  className: 'z-50',
+  delay: 100,
+});
+
+Router.events.on('routeChangeStart', progress.start);
+Router.events.on('routeChangeComplete', progress.finish);
+Router.events.on('routeChangeError', progress.finish);
 
 function MyApp({ Component, pageProps }) {
   return (
