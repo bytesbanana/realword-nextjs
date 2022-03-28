30609d95d0c126184d904239a7a59f0279eadaab
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-03-28 11:09:56 +0700

    add variant for popular tag

--- components/PopularTags.js
+++ components/PopularTags.js
@@ -1,15 +1,12 @@
 import React from 'react';
-import Link from 'next/link';
-import { useSelector } from 'react-redux';
 import TagList from './TagList';
 
 const PopularTags = (props) => {
-  const loading = useSelector((state) => state.app.loading);
   return (
     <div className='col-md-3'>
       <div className='sidebar'>
         <p>Popular Tags</p>
-        <TagList tags={props.tags} />
+        <TagList tags={props.tags} variant="secondary"/>
       </div>
     </div>
   );
