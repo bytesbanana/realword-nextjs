a4285eb7e38fd1e0a983d22875887db749bf6bc7
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-08 20:20:59 +0700

    start over

--- next.config.js
+++ next.config.js
@@ -1,6 +1,6 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
-  env: {
-    API_BASE_URL: process.env.API_BASE_URL,
-  },
-};
+}
+
+module.exports = nextConfig
