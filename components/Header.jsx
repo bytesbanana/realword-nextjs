bae16715ca7ee55a9675df2054fd7192cecaf646
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-10 15:39:17 +0700

    render menu by conditions

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,6 +1,7 @@
+import React from 'react';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
-import React from 'react';
+import { useSession } from 'next-auth/react';
 
 const menus = [
   {
@@ -8,28 +9,33 @@ const menus = [
     pathName: '/',
   },
   {
-    name: 'About',
-    pathName: '/about',
+    name: 'New Article',
+    pathName: '/editor',
     icon: 'ion-compose',
+    auth: true,
   },
 
   {
     name: 'Settings',
     pathName: '/settings',
     icon: 'ion-gear-a',
+    auth: true,
   },
   {
     name: 'Sign in',
     pathName: '/login',
+    auth: false,
   },
   {
     name: 'Sign up',
     pathName: '/register',
+    auth: false,
   },
 ];
 
 const Header = () => {
   const router = useRouter();
+  const { data: session } = useSession();
 
   return (
     <nav className='navbar navbar-light'>
@@ -38,19 +44,42 @@ const Header = () => {
           conduit
         </a>
         <ul className='nav navbar-nav pull-xs-right'>
-          {menus.map(({ name, pathName, icon = null }) => (
-            <Link href={pathName} key={name} passHref>
-              <li className='nav-item'>
-                <a
-                  className={`nav-link ${
-                    router.pathname === pathName ? 'active' : ''
-                  }`}
-                >
-                  {icon && <i className={icon}></i>} {name}
-                </a>
-              </li>
-            </Link>
-          ))}
+          {menus
+            .filter((menu) => {
+              return (menu.auth && !!session) || menu?.auth === undefined;
+            })
+            .map(({ name, pathName, icon = null }) => (
+              <Link href={pathName} key={name} passHref>
+                <li className='nav-item'>
+                  <a
+                    className={`nav-link ${
+                      router.pathname === pathName ? 'active' : ''
+                    }`}
+                  >
+                    {icon && <i className={icon}></i>} {name}
+                  </a>
+                </li>
+              </Link>
+            ))}
+
+          <Link
+            href={`/profile/${session?.user.username}`}
+            key={session?.user?.username}
+            passHref
+          >
+            <li className='nav-item'>
+              <a
+                className={`nav-link ${router.pathname.includes('/profile')}`}
+              >
+                <img
+                  className='user-pic'
+                  src={`${session?.user.image}`}
+                  alt={session?.user?.username}
+                />
+                {session?.user.username}
+              </a>
+            </li>
+          </Link>
         </ul>
       </div>
     </nav>
