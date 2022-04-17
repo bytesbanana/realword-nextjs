dcf0e701cf9820ea6e2d30626a81275231ce6c78
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-17 23:54:03 +0700

    initila home page

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,6 +1,44 @@
+import { useEffect, useState } from 'react';
 import Link from 'next/link';
 
-const Home = () => {
+import { withSessionSsr } from 'lib/session';
+import ArticlesAPI from 'lib/api/ArticlesAPI';
+import TagsAPI from 'lib/api/TagsAPI';
+
+import ArticleList from 'components/ArticleList';
+import PopularTags from 'components/PopularTags';
+
+const FeedType = {
+  GLOBAL: 'GLOBAL',
+  YOUR_FEED: 'YOUR_FEED',
+};
+
+const Home = ({ user, articles: tempArticles, tagList }) => {
+  const [articles, setArticles] = useState(tempArticles);
+  const [currentFeed, setCurrentFeed] = useState(FeedType.GLOBAL);
+  const [tag, setTag] = useState(null);
+  const [loadingArticles, setLoadingArticles] = useState(false);
+
+  useEffect(() => {
+    if (currentFeed === FeedType.GLOBAL) {
+      setTag(null);
+    }
+  }, [currentFeed]);
+
+  const handleTagSelect = (tag) => {
+    setTag(tag);
+  };
+
+  const handleGlobalFeedClick = async () => {
+    if (loadingArticles) return;
+    setLoadingArticles(true);
+    setTag(null);
+    setCurrentFeed(FeedType.GLOBAL);
+    const newArticles = await ArticlesAPI.getArticles();
+    setArticles(newArticles);
+    setLoadingArticles(false);
+  };
+
   return (
     <>
       <div className='home-page'>
@@ -15,80 +53,38 @@ const Home = () => {
             <div className='col-md-9'>
               <div className='feed-toggle'>
                 <ul className='nav nav-pills outline-active'>
+                  {user && (
+                    <li className='nav-item'>
+                      <Link href='/'>
+                        <a className='nav-link disabled'>Your Feed</a>
+                      </Link>
+                    </li>
+                  )}
                   <li className='nav-item'>
-                    <Link href={''}>
-                      <a className='nav-link disabled'>Your Feed</a>
-                    </Link>
+                    <a
+                      className={`nav-link ${
+                        currentFeed === FeedType.GLOBAL && !tag ? 'active' : ''
+                      }`}
+                      style={{ cursor: 'pointer', userSelect: 'none' }}
+                      onClick={handleGlobalFeedClick}
+                    >
+                      Global Feed
+                    </a>
                   </li>
-                  <li className='nav-item'>
-                    <Link href={''}>
-                      <a className='nav-link active'>Global Feed</a>
-                    </Link>
-                  </li>
+                  {tag && (
+                    <li className='nav-item'>
+                      <a className={`nav-link ${tag ? 'active' : ''}`}>
+                        # {tag}
+                      </a>
+                    </li>
+                  )}
                 </ul>
               </div>
-              <div className='article-preview'>
-                <div className='article-meta'>
-                  <Link href='profile.html'>
-                    <a>
-                      <img src='http://i.imgur.com/Qr71crq.jpg' />
-                    </a>
-                  </Link>
-                  <div className='info'>
-                    <Link href={''}>
-                      <a className='author'>Eric Simons</a>
-                    </Link>
-                    <span className='date'>January 20th</span>
-                  </div>
-                  <button className='btn btn-outline-primary btn-sm pull-xs-right'>
-                    <i className='ion-heart' /> 29
-                  </button>
-                </div>
-                <Link href={''}>
-                  <a href={''} className='preview-link'>
-                    <h1>How to build webapps that scale</h1>
-                    <p>This is the description for the post.</p>
-                    <span>Read more...</span>
-                  </a>
-                </Link>
-              </div>
-              <div className='article-preview'>
-                <div className='article-meta'>
-                  <Link href='profile.html'>
-                    <a>
-                      <img src='http://i.imgur.com/N4VcUeJ.jpg' />
-                    </a>
-                  </Link>
-                  <div className='info'>
-                    <Link href={''}>
-                      <a className='author'>Albert Pai</a>
-                    </Link>
-                    <span className='date'>January 20th</span>
-                  </div>
-                  <button className='btn btn-outline-primary btn-sm pull-xs-right'>
-                    <i className='ion-heart' /> 32
-                  </button>
-                </div>
-                <Link href={''}>
-                  <a className='preview-link'>
-                    <h1>
-                      The song you won't ever stop singing. No matter how hard
-                      you try.
-                    </h1>
-                    <p>This is the description for the post.</p>
-                    <span>Read more...</span>
-                  </a>
-                </Link>
-              </div>
+              <ArticleList articles={articles} isLoading={loadingArticles} />
             </div>
             <div className='col-md-3'>
               <div className='sidebar'>
-                <p>Popular Tags</p>
-                <div className='tag-list'>
-                  <Link href={''}>
-                    <a className='tag-pill tag-default'>programming</a>
-                  </Link>
-                </div>
+                <PopularTags tagList={tagList} onTagSelect={handleTagSelect} />
               </div>
             </div>
           </div>
@@ -97,5 +93,20 @@ const Home = () => {
     </>
   );
 };
+
+export const getServerSideProps = withSessionSsr(async ({ req }) => {
+  const user = req?.session?.user;
+
+  const articles = await ArticlesAPI.getArticles();
+  const tagList = await TagsAPI.getTags();
+
+  return {
+    props: {
+      user: user || null,
+      articles: articles || [],
+      tagList: tagList || [],
+    },
+  };
+});
 
 export default Home;
