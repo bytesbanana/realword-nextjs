33edc01341dd1e695868494714ec3d2093aededa
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-18 21:07:49 +0700

    Implement custom document component

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,13 +1,11 @@
 import '../styles/globals.css';
 
-import Head from '../components/Head';
 import Header from '../components/Header';
 import { AuthProvider } from 'contexts/AuthContext';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
-      <Head />
       <Header />
       <Component {...pageProps} />
     </AuthProvider>
