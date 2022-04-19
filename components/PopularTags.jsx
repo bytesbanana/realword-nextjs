e6d9e4d6e05d907ede1784a6c5b2a824d0641c00
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-19 16:26:53 +0700

    implement fetch popular tags

--- components/PopularTags.jsx
+++ components/PopularTags.jsx
@@ -1,20 +1,27 @@
+import TagsAPI from 'lib/api/TagsAPI';
 import Link from 'next/link';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
-const PopularTags = ({ tagList = [], onTagSelect = () => {} }) => {
+const PopularTags = () => {
+  const [loading, setLoading] = useState(true);
+  const [tagList, setTagList] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      setTagList(await TagsAPI.getTags());
+      setLoading(false);
+    })();
+  }, []);
+
   return (
     <>
       <p>Popular Tags</p>
       <div className='tag-list'>
+        {loading && <p>Loading tags...</p>}
         {tagList.map((tag) => (
-          <a
-            key={tag}
-            className={`tag-default tag-pill`}
-            onClick={() => onTagSelect(tag)}
-            href='#'
-          >
-            {tag}
-          </a>
+          <Link key={tag} href={`/?tag=${tag}`} shallow>
+            <a className='tag-default tag-pill'>{tag}</a>
+          </Link>
         ))}
       </div>
     </>
