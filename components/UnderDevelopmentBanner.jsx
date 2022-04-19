9fa0253a3eae6690c72c49af0a962b923d6016ed
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-19 17:38:21 +0700

    Add under development banner

---
+++ components/UnderDevelopmentBanner.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import * as styles from './UnderDevelopmentBanner.module.css';
+
+const ImplementRibbon = () => {
+  return (
+    <div className={styles.ribbon}>
+      This website is currently under development.
+    </div>
+  );
+};
+
+export default ImplementRibbon;
--- pages/_app.jsx
+++ pages/_app.jsx
@@ -2,10 +2,12 @@ import '../styles/globals.css';
 
 import Header from '../components/Header';
 import { AuthProvider } from 'contexts/AuthContext';
+import ImplementRibbon from 'components/UnderDevelopmentBanner';
 
 function MyApp({ Component, pageProps }) {
   return (
     <AuthProvider>
+      <ImplementRibbon />
       <Header />
       <Component {...pageProps} />
     </AuthProvider>
