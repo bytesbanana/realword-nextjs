ebe02e328cb546fc49ff5a8f4abe9b69d87325a7
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-21 15:40:27 +0700

    protect your feed from unauthuriaztion request

--- pages/index.jsx
+++ pages/index.jsx
@@ -2,9 +2,22 @@ import ArticleList from 'components/ArticleList';
 import Banner from 'components/Banner';
 import FeedToggle from 'components/FeedToggle';
 import PopularTags from 'components/PopularTags';
-import React from 'react';
+import AuthContext from 'contexts/AuthContext';
+import { useRouter } from 'next/router';
+import React, { useContext, useEffect } from 'react';
 
 const Home = () => {
+  const router = useRouter();
+  const { isAuthenticated } = useContext(AuthContext);
+
+  useEffect(() => {
+    const { follow } = router.query;
+    if (!router.isReady) return;
+    if (!isAuthenticated && follow) {
+      router.push('/');
+    }
+  }, [isAuthenticated, router]);
+
   return (
     <div className='home-page'>
       <Banner />
