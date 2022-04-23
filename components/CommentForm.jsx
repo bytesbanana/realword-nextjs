c609d8d86d52adba592925fe8100a6cc541a8f2c
Saharat Paynok <34708159+ByteBanana@users.noreply.github.com>
2022-04-23 12:39:10 +0700

    Add comment form components

---
+++ components/CommentForm.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const CommentForm = () => {
+  return (
+    <form className='card comment-form'>
+      <div className='card-block'>
+        <textarea
+          className='form-control'
+          placeholder='Write a comment...'
+          rows={3}
+          defaultValue={''}
+        />
+      </div>
+      <div className='card-footer'>
+        <img
+          src='http://i.imgur.com/Qr71crq.jpg'
+          className='comment-author-img'
+          alt='commentor'
+        />
+        <button className='btn btn-sm btn-primary'>Post Comment</button>
+      </div>
+    </form>
+  );
+};
+
+export default CommentForm;
